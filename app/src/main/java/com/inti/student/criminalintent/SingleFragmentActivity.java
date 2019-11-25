ebd91eb033b67e62cb3de490b5580568188f59fc
wayneng6010 <wayne.ng6010@gmail.com>
2019-11-25 23:06:29 +0800

    interface enhance, added action bar, linked to cart

--- app/src/main/java/com/inti/student/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/inti/student/criminalintent/SingleFragmentActivity.java
@@ -16,12 +16,17 @@ public abstract class SingleFragmentActivity extends FragmentActivity {
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
         if(fragment == null) {
             fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
+
+    }
+
+    public void goToCart(android.view.View v) {
+        android.content.Intent intent = new android.content.Intent(getBaseContext(), CartActivity.class);
+        startActivity(intent);
     }
 }
